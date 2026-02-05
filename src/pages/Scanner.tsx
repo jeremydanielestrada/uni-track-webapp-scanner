a9c4b3a648c7a405cee7e4d0f935c79062693ddd
Jdestrada <jeremydanielestrada2001@gmail.com>
2026-02-05 20:10:27 +0800

    Add QRScanner component and configure camera access; update Scanner page integration

--- src/pages/Scanner.tsx
+++ src/pages/Scanner.tsx
@@ -1,4 +1,4 @@
-import QRScanner from "../components/system/QRscanner";
+import QRScanner from "../components/QRScanner";
 
 import { useState } from "react";
 
@@ -6,7 +6,7 @@ export default function Scanner() {
   const [data, setData] = useState<string | null>(null);
   return (
     <>
-      <QRScanner onScan={(data) => setData(data)} />
+      <QRScanner onScan={(data: string) => setData(data)} />
       <p>{data || "no data"}</p>
     </>
   );
