95803af5fb9d558fe3606435b75869aed67fb3e6
Jdestrada <jeremydanielestrada2001@gmail.com>
2026-02-05 17:29:11 +0800

    Add axios integration and logo to Authorize page
    - Introduced axios for API calls in a new axios.ts file.
    - Updated package.json and package-lock.json to include axios dependency.
    - Added logo image to the public/images directory.
    - Enhanced Authorize component with loading state and form handling for student authorization.

--- src/pages/Authorize.tsx
+++ src/pages/Authorize.tsx
@@ -1,15 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+import { api } from "../api/axios";
+import { useNavigate } from "react-router";
+
+type Student = {
+  id_num: string;
+  event_code: string;
+};
 export default function Authorize() {
+  const [isLoading, setIsloading] = useState<boolean>(false);
+  const [student, setStudent] = useState<Student>({
+    id_num: "",
+    event_code: "",
+  });
+  const navigate = useNavigate();
+
+  const authAssignedStudent = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setIsloading(true);
+      const res = await api.post("/students/authorize");
+
+      if (res.data?.authorized) {
+        navigate("/scanner");
+      }
+    } catch (error: any) {
+      console.log(error.response.data.message);
+    } finally {
+      setIsloading(false);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setStudent({ id_num: "", event_code: "", [name]: value });
+  };
   return (
-    <div className="bg-card shadow-lg rounded-lg p-8 w-80 md:w-100">
-      <form>
-        <div className="flex flex-col space-y-2 my-3">
+    <div className="bg-card shadow-lg rounded-lg p-6 w-80 md:min-w-100">
+      <div className="w-50 mx-auto">
+        <img
+          src="/public/images/logo.png"
+          alt="Logo"
+          className="object-cover"
+        />
+      </div>
+      <form onClick={authAssignedStudent}>
+        <div className="flex flex-col space-y-2 mb-3">
           <label htmlFor="id-numeberr" className="text-secondary font-semibold">
             ID-Number
           </label>
           <input
             type="text"
-            placeholder="eg. 200-00000"
             className=" p-2 border border-secondary-foreground  rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
+            name="id-number"
+            onChange={handleChange}
           />
         </div>
 
@@ -19,15 +63,17 @@ export default function Authorize() {
           </label>
           <input
             type="text"
-            placeholder="eg. 200-00000"
             className=" p-2 border border-secondary-foreground  rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
+            name="event code"
+            onChange={handleChange}
           />
         </div>
         <button
           type="submit"
-          className="w-full rounded-lg py-3 px-4 bg-primary text-white font-bold cursor-pointer hover:bg-primary/50"
+          className={`${isLoading ? "opacity-70" : "bg-primary text-white font-bold cursor-pointer hover:bg-primary/50"} w-full rounded-lg py-3 px-4 `}
+          disabled={isLoading}
         >
-          Proceed
+          {isLoading ? "Authorizing..." : "Authorize"}
         </button>
       </form>
     </div>
